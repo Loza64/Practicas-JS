145522a3604eecaefded6e96a7bf8700ac4cae55
Roberto Loza <76535183+Loza64@users.noreply.github.com>
2024-01-25 22:53:04 -0600

    changes

---
+++ Kotlin/app/src/main/java/com/loza/application/Product.kt
@@ -0,0 +1,24 @@
+package com.loza.application
+
+class Product private constructor(val name:String, val stock:Int, val price:Double){
+
+    companion object{
+        fun getProduct(name:String, stock:Int, price:Double):Product{
+            return Product(name, stock, price)
+        }
+
+        fun getDetails():String{
+            return "This is a product"
+        }
+    }
+}
+
+
+fun main(){
+    //Devido a que es un contructor privado, la instancia no puede ser Product().funcion
+    val product = Product.getProduct("Play", 34, 756.34)
+    println(Product.getDetails())
+    println("name: ${product.name}")
+    println("stock: ${product.stock}")
+    println("price: ${product.price}")
+}
