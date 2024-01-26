74d5ef8e3b739fe0342ccb81edf7ebb9dcdece04
Roberto Loza <robertoloxa88@gmail.com>
2024-01-26 16:40:33 -0600

    Finish practice List and MutableList

--- Kotlin/app/src/main/java/com/loza/application/List.kt
+++ Kotlin/app/src/main/java/com/loza/application/List.kt
@@ -2,22 +2,45 @@ package com.loza.application
 
 fun main(){
 
-    println("--------------------------------Original list")
-    var lists:MutableList<String> = mutableListOf("Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo")
-    println(lists.joinToString())
+    println("--------------------------------Original MutableList")
+    var listMutable:MutableList<String> = mutableListOf("Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo")
+    println(listMutable.joinToString())
 
-    println("--------------------------------Add new element with any index")
-    lists.add(0,"holaaaaaa")
-    println(lists.joinToString())
+    println("--------------------------------Add new element with any index using .add")
+    listMutable.add(0,"holaaaaaa with add, (index:0, element: holaaaaaaaaaaaa)")
+    listMutable.plus("holaaaaaaaa using plus")
+    println(listMutable.joinToString())
 
     println("--------------------------------Remove item from list")
-    lists = lists.filter { it != "Lunes" }.toMutableList()
-    println(lists.joinToString())
+    listMutable = listMutable.filter { it != "Lunes" }.toMutableList()
+    println(listMutable.joinToString())
 
     println("--------------------------------Alter list")
-    lists.forEachIndexed { index, element ->
-        lists[index] = if (element == "Lunes") "peppa" else element
+    listMutable.forEachIndexed { index, element ->
+        listMutable[index] = if (element == "Lunes") "peppa" else element
     }
 
-    println(lists.joinToString())
+    println(listMutable.joinToString())
+
+    println("--------------------------------Original NormalList")
+
+    var normalList:List<String> = listOf("Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo")
+    println(normalList.joinToString())
+
+    println("-----------------Add new element")
+    normalList.plus("New element using plus")
+    println(normalList.joinToString())
+
+    println("-----------------Remove element using filter (Remove Lunes)")
+    normalList = normalList.filter { it !== "Lunes" }
+    println(normalList.joinToString ())
+
+    println("-----------------Alter element Martes to Peppa")
+    val index = 1
+    normalList = normalList.toMutableList().apply {
+        this[index] = "Peppa"
+    }
+    println(normalList.joinToString())
+
+    println("\n Me gusta más MutableList que List")
 }
