4a84172679f51403c65ade8ef7394485f6bb9810
Roberto Loza <76535183+Loza64@users.noreply.github.com>
2024-01-29 22:12:00 -0600

    Practice Layout Consttraint

--- Kotlin/app/src/main/java/com/loza/application/Activities/SecondActivity.kt
+++ Kotlin/app/src/main/java/com/loza/application/Activities/SecondActivity.kt
@@ -1,11 +1,12 @@
-package com.loza.application
+package com.loza.application.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.loza.application.R
 
-class MainActivity : AppCompatActivity() {
+class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_second)
     }
 }
