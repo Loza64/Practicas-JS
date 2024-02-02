469755655f4030e5027685523e093525249237f5
Roberto Loza <76535183+Loza64@users.noreply.github.com>
2024-02-01 22:12:51 -0600

    changes

--- Kotlin/app/src/main/java/com/loza/application/Activities/ThirdActivity.kt
+++ Kotlin/app/src/main/java/com/loza/application/Activities/ThirdActivity.kt
@@ -2,11 +2,15 @@ package com.loza.application.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.EditText
 import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
 import com.loza.application.R
 
 class ThirdActivity : AppCompatActivity(), View.OnClickListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_third)
@@ -18,7 +22,12 @@ class ThirdActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(p0: View?) {
         when(p0?.id){
             R.id.btnStart -> {
-
+                val txtInfo = findViewById<AppCompatEditText>(R.id.txtInfo)
+                if(txtInfo.text.toString().isEmpty()){
+                    Log.i("Application", "Button pressed")
+                }else{
+                    Log.i("Application", "Button with info: ${txtInfo?.text.toString()}")
+                }
             }
         }
     }
