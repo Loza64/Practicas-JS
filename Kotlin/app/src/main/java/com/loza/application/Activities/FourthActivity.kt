d29322ba633c66427575c6870959dfe28cd575c3
Roberto Loza <76535183+Loza64@users.noreply.github.com>
2024-02-03 19:19:00 -0600

    New ACtivity kotlin

---
+++ Kotlin/app/src/main/java/com/loza/application/Activities/FourthActivity.kt
@@ -0,0 +1,12 @@
+package com.loza.application.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.loza.application.R
+
+class FourthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fourth)
+    }
+}
