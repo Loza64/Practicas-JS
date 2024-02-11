1121b8aefa94cd27365a2b37789d740a0b86ca50
Roberto Loza <robertoloxa88@gmail.com>
2024-02-10 19:06:45 -0600

    Finish training

--- Kotlin/app/src/main/java/com/loza/application/Activities/exercices/ImcCalculatorActivity.kt
+++ Kotlin/app/src/main/java/com/loza/application/Activities/exercices/ImcCalculatorActivity.kt
@@ -5,34 +5,76 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.widget.AppCompatButton
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.slider.RangeSlider
 import com.loza.application.R
 import java.text.DecimalFormat
+import kotlin.math.pow
 
 class ImcCalculatorActivity : AppCompatActivity() {
 
     private var maleSelected = true
     private var femaleSelected = false
+    private var height = 120.0f
+    private var weight = 60
+    private var age = 24
+    val imc = weight / (height/100).pow(2) // Peso/(Altura en m al cuadrado)
 
     private lateinit var cardMale:CardView
     private lateinit var cardFemale:CardView
+
     private lateinit var tvHeight:TextView
     private lateinit var rsHeight:RangeSlider
+
+    private lateinit var tvWeight:TextView
+    private lateinit var tvAge:TextView
+    private lateinit var btnWeightMin:FloatingActionButton
+    private lateinit var btnWeightPlus:FloatingActionButton
+    private lateinit var btnAgeMin:FloatingActionButton
+    private lateinit var btnAgePlus:FloatingActionButton
+
+    private lateinit var btnCalculateImc:AppCompatButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imc_calculator)
         initUI()
+    }
 
+    private fun showAlertDialog(message:String) {
+        val builder = AlertDialog.Builder(this)
+
+        builder.setTitle("Calculadora Imc")
+        builder.setMessage(message)
+
+        // Código a ejecutar cuando se hace clic en el botón "Aceptar"
+        builder.setPositiveButton("Accept") { dialog, which ->
+            dialog.dismiss() // Cerrar el cuadro de diálogo
+        }
+
+        val dialog = builder.create()
+        dialog.show()
     }
 
     private fun initComponents(){
         cardMale = findViewById(R.id.cardMale)
         cardFemale = findViewById(R.id.cardFemale)
+
         tvHeight = findViewById(R.id.tvHeight)
         rsHeight = findViewById(R.id.rsHeight)
+
+        tvWeight = findViewById(R.id.tvWeight)
+        tvAge = findViewById(R.id.tvAge)
+        btnWeightMin = findViewById(R.id.btnWeightMin)
+        btnWeightPlus = findViewById(R.id.btnWeightPlus)
+        btnAgeMin = findViewById(R.id.btnAgeMin)
+        btnAgePlus = findViewById(R.id.btnAgePlus)
+
+        btnCalculateImc = findViewById(R.id.btnCalculateImc)
     }
 
     private fun getBackGround(isSelected:Boolean): Int{
@@ -55,15 +97,58 @@ class ImcCalculatorActivity : AppCompatActivity() {
             femaleSelected = false
             setColor();
         }
+
         cardFemale.setOnClickListener{
             maleSelected = false
             femaleSelected = true
             setColor();
         }
+
         rsHeight.addOnChangeListener { _, value, _ ->
             val df = DecimalFormat("#.##")
-            var result = df.format(value)
+            height = value
+            var result = df.format(height)
             tvHeight.text = "$result cm"
+        }
+
+        btnWeightMin.setOnClickListener{
+            if(weight < 18){
+                weight = 18
+            }else{
+                weight -=1
+            }
+            tvWeight.text = weight.toString()
+        }
+
+        btnWeightPlus.setOnClickListener{
+            weight +=1
+            tvWeight.text = weight.toString()
+        }
+
+        btnAgeMin.setOnClickListener {
+            if(age < 18){
+                age = 18
+            }else{
+                age -=1
+            }
+            tvAge.text = age.toString()
+        }
+
+        btnAgePlus.setOnClickListener {
+            age +=1
+            tvAge.text = age.toString()
+        }
+
+        btnCalculateImc.setOnClickListener {
+            if(imc < 18.5){
+                showAlertDialog("Su peso es bajo")
+            }else if(imc > 18.5 && imc <= 24.9 ){
+                showAlertDialog("Su peso es normal")
+            }else if(imc < 25 && imc <= 29.9){
+                showAlertDialog("Usted sufre de sobre peso")
+            }else if(imc > 30){
+                showAlertDialog("Usted sufre de obesidad")
+            }
         }
     }
 
